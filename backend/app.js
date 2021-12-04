7d6da7842456887286b22dd815a2d7b6b561c51e
tforde4623 <sltebr33zy@gmail.com>
2021-12-03 23:27:27 -0600

    added some error handling code for backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -35,5 +36,38 @@ app.use(
 );
 
 app.use(require('./routes'));
+
+// "404" err handler
+app.use((_req, _res, next) => {
+  const err = new Error('The requested resource could not be found.');
+  err.title = "Resource Not Found";
+  err.errors = ['The requested resource could not be found.'];
+  err.status = 404;
+  next(err);
+});
+
+// sequelize error 'processing'
+app.use((err, _req, _res, next) => {
+  // check error type
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message);
+    err.title = 'Validation error';
+  }
+  next(err)
+});
+
+// format errors for json response
+app.use((err, _req, res, _next) => {
+  // if it is not an error we have assigned prev, send generic internal err
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    // if server is in prod we don't want to send the stack
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
