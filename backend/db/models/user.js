bf51e94621e9f8e2bcd1bcf88ab2e139ef0c4552
tforde4623 <sltebr33zy@gmail.com>
2021-12-04 00:16:15 -0600

    added scopes to user model

---
+++ backend/db/models/user.js
@@ -0,0 +1,53 @@
+'use strict';
+const { Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+        isNotEmail(val) {
+          if (Validator.isEmail(val)) {
+            throw new Error('Cannot be an email.');
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 256]
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  }, 
+  {
+    // determin what information to return at different times to protect sensative data
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ['hashedPassword'] }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
+  });
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
