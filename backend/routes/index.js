174a0bf9e32714a89a5853e20e011032b6a48028
tforde4623 <sltebr33zy@gmail.com>
2021-12-03 23:10:16 -0600

    test routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get(
-  '/hello/world',
-  (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('hello world!');
-  }
-);
+router.use('/api', require('./api'));
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 module.exports = router;
