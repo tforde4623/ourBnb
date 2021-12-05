2afce30d7b683e6af1415dd19bf1ad5e16bed647
tforde4623 <sltebr33zy@gmail.com>
2021-12-04 20:51:08 -0600

    did lots of work on validating inputs for api routes (for correct inputs, not yet validating not vuln inputs

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 // middlware to format and return validation errors "prettily"
 const handleValidationErrors = (req, _res, next) => {
@@ -19,6 +19,40 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
-module.exports = {
+// express validation arrays
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
   handleValidationErrors
+];
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+module.exports = {
+  handleValidationErrors,
+  validateLogin,
+  validateSignup
 };
