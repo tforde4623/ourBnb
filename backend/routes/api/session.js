95a3bbfeefcfc8dc3496342f940e7dd84347d5ad
tforde4623 <sltebr33zy@gmail.com>
2021-12-04 19:40:27 -0600

    added login/logout functionality (for the api/session)

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,4 +31,13 @@ router.post(
   })
 );
 
+// log out - DELETE '/api/session'
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 module.exports = router;
